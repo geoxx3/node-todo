d9a3300e05a24174ab29eb985a74158c72f22375
geoxx3 <geoxx3@gmail.com>
2017-11-16 22:00:15 +0000

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,16 +1,33 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-  id: 10
-};
+var password = '123abc!';
 
-var token = jwt.sign(data, '123abc');
-console.log(token);
+// bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(password, salt, (err, hash) => {
+//     console.log(hash);
+//   });
+// });
 
-var decoded = jwt.verify(token, '123abc');
-console.log('decoded', decoded);
+var hashedPassword = '$2a$10$TttwBNmOtrCOE5AKwW0bgeFhK5PoaOfim9Es1Qh38948iBx6CR7mm';
 
+bcrypt.compare('123abc!', hashedPassword, (err, res) => {
+  console.log(res);
+})
+
+
+
+
+// var data = {
+//   id: 10
+//
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
+//
+// var decoded = jwt.verify(token, '123abc');
+// console.log('decoded', decoded);
+//
 
 
 
